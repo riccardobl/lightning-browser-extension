3ff30b42c9088b3efa48abae335f76bf26ef4171
escapedcat <github@htmlcss.de>
2022-05-31 14:12:45 +0800

    refactor(state): isUnlocked handling #652
    chore: remove unsued code

--- src/extension/background-script/state.ts
+++ src/extension/background-script/state.ts
@@ -10,17 +10,18 @@ import type { Account, Accounts, SettingsStorage } from "~/types";
 import i18n from "~/i18n/i18nConfig";
 
 interface State {
-  connector: Connector | null;
   account: Account | null;
-  settings: SettingsStorage;
   accounts: Accounts;
+  connector: Connector | null;
   currentAccountId: string | null;
-  password: string | null;
   getAccount: () => Account | null;
   getConnector: () => Promise<Connector>;
-  lock: () => Promise<void>;
   init: () => Promise<void>;
+  isUnlocked: () => boolean;
+  lock: () => Promise<void>;
+  password: string | null;
   saveToStorage: () => Promise<void>;
+  settings: SettingsStorage;
 }
 
 interface BrowserStorage {
